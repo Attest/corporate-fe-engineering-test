a80a55e096cc5ad0eb0462f8216e2ce6b2207eaf
Slavco86 <slavick.slavick@gmail.com>
2019-11-19 13:17:22 +0000

    Map answer text to answer id in results page

--- src/components/SurveyResults.vue
+++ src/components/SurveyResults.vue
@@ -2,7 +2,7 @@
 	<div class="results">
 		<div class="results-item" v-for="(question, index) in results" :key="index">
 			<p>{{question.title}}</p>
-			<progress-bar v-for="(percent, answer, i) in question.answers" :fill="percent" :label="answer" :key="i"></progress-bar>
+			<progress-bar v-for="(answer, i) in question.answers" :fill="answer.value" :label="answer.text" :key="i"></progress-bar>
 		</div>
 	</div>
 </template>
@@ -10,7 +10,7 @@
 <script>
 	import Vue from 'vue'
   import {mapGetters} from "vuex";
-  import SurveyResponse from "../models/surveys/responses";
+	import SurveyResponse, {SurveyAnswer} from "../models/surveys/responses";
   import ProgressBar from "@/components/ProgressBar";
 
 	export default Vue.extend({
@@ -32,7 +32,7 @@
 	    results() {
 		    return this.questionsData.map((question, index) => new SurveyResponse({
 			    title: question.title,
-			    answers: this.responses[index]
+			    answers: Object.entries(this.responses[index]).map(entry => new SurveyAnswer(entry, this.questionsData[index].answers))
 		    }))
 	    }
     }
--- src/models/surveys/responses.ts
+++ src/models/surveys/responses.ts
@@ -1,13 +1,26 @@
 import {Answer} from "@/models/questions/questions";
-import {ISurveyResponse} from "@/models/department/department";
 
 export default class SurveyResponse {
     title: string
-    answers: ISurveyResponse[]
+    answers: SurveyAnswer[]
 
     constructor(data: SurveyResponse) {
         this.title = data.title
         this.answers = data.answers
     }
+}
 
+export class SurveyAnswer extends Answer {
+    value: number
+
+    constructor(entries: [string, number], answerMap: Answer[]) {
+        let matchedAnswerType: Answer | undefined
+        matchedAnswerType = answerMap.find(answer => answer.id === entries[0])
+
+        super({
+            id: entries[0],
+            text: matchedAnswerType ? matchedAnswerType.text : entries[0]
+        })
+        this.value = entries[1]
+    }
 }
